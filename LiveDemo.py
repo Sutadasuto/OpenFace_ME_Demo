3f147fd8834e14cef03a417d32654966d966e6d1
Rodrigo Rill-García <39140700+Sutadasuto@users.noreply.github.com>
2020-01-16 14:25:56 +0100

    Terminal support added.

--- LiveDemo.py
+++ LiveDemo.py
@@ -5,14 +5,23 @@ import datetime
 from tkinter import *
 from graphs import gui
 
-outputFolder = "/home/sutadasuto/DemoOpenFace"
 
-outputFolder += ("/" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("openface_path", type=str)
+    parser.add_argument("--output_folder", type=str, default=os.path.join(os.getcwd(), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+    parser.add_argument("--device_id", type=int, default=0)
+    
 
-command = '/home/sutadasuto/OpenFace/build/bin/FeatureExtraction -device 0 ' \
-          '-vis-aus -pose -aus -gaze -simalign -tracked -g -out_dir "' + outputFolder + '"'
-os.system(command)
+def main(args):
+    command = '%s -device %s -vis-aus -pose -aus -gaze -simalign -tracked -g -out_dir "%s"' % 
+          (os.path.join(args.openface_path, "build", "bin", "FeatureExtraction").replace(" ", "\ "), args.device_id, args.output_folder)
+    os.system(command)
  
+    app = gui(outputFolder)
+    app.mainloop()
 
-app = gui(outputFolder)
-app.mainloop()
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
